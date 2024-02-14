e81958283d40619a12f9a48d4c81f8fd3e778ea2
opixelum <48217573+opixelum@users.noreply.github.com>
2024-02-14 22:19:44 +0100

    Format `Display.py`

--- Display.py
+++ Display.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog
 import fileParser
 import Main
+
 
 def OpenWindow(size, title):
     w = tk.Tk()
@@ -11,47 +11,55 @@ def OpenWindow(size, title):
 
 
 def createTable():
-    numRows = len(Main.context['array']) + 1 if len(Main.context['array']) + 1 > 10 else 10
-    numColumns = len(Main.context['array'][0])
+    numRows = (
+        len(Main.context["array"]) + 1 if len(Main.context["array"]) + 1 > 10 else 10
+    )
+    numColumns = len(Main.context["array"][0])
     for i in range(numColumns):
         Main.window.grid_columnconfigure(i, weight=1)
     for i in range(numRows):
         Main.window.grid_rowconfigure(i, weight=1)
 
-    for i, value in enumerate(Main.context['array'][0]):
+    for i, value in enumerate(Main.context["array"][0]):
         tmp = value
-        if tmp == Main.context['sortKey']:
-            if Main.context['sortReverse']:
-                tmp += ' ▼'
+        if tmp == Main.context["sortKey"]:
+            if Main.context["sortReverse"]:
+                tmp += " ▼"
             else:
-                tmp += ' ▲'
+                tmp += " ▲"
         button = tk.Button(Main.window, text=tmp, command=lambda x=value: sortArray(x))
         button.grid(row=0, column=i)
 
-    for i in range(0, len(Main.context['array'])):
-        for j, value in enumerate(Main.context['array'][i]):
-            label = tk.Label(Main.window, text=str(Main.context['array'][i][value]))
+    for i in range(0, len(Main.context["array"])):
+        for j, value in enumerate(Main.context["array"][i]):
+            label = tk.Label(Main.window, text=str(Main.context["array"][i][value]))
             label.grid(row=i + 1, column=j)
 
 
 def openFile():
     file = tk.filedialog.askopenfile()
-    Main.context = {'array': [], 'sortKey': '', 'sortReverse': False, 'file': '', 'columns': []}
+    Main.context = {
+        "array": [],
+        "sortKey": "",
+        "sortReverse": False,
+        "file": "",
+        "columns": [],
+    }
     if file:
         # get type of file csv, json, xml, yaml
-        ext = file.name.split('.')[-1]
-        Main.context['file'] = file.name
-        if ext == 'csv':
-            Main.context['array'] = fileParser.csvToArray(file.name)
-        elif ext == 'json':
-            Main.context['array'] = fileParser.jsonFileToArray(file.name)
-        elif ext == 'xml':
-            Main.context['array'] = fileParser.xmlToArray(file.name)
-        elif ext == 'yaml':
-            Main.context['array'] = fileParser.yamlToArray(file.name)
+        ext = file.name.split(".")[-1]
+        Main.context["file"] = file.name
+        if ext == "csv":
+            Main.context["array"] = fileParser.csvToArray(file.name)
+        elif ext == "json":
+            Main.context["array"] = fileParser.jsonFileToArray(file.name)
+        elif ext == "xml":
+            Main.context["array"] = fileParser.xmlToArray(file.name)
+        elif ext == "yaml":
+            Main.context["array"] = fileParser.yamlToArray(file.name)
         else:
-            print('ERROR : File type not supported')
-            Main.context['array'] = []
+            print("ERROR : File type not supported")
+            Main.context["array"] = []
         fileParser.getColumns(Main.context)
         displayArray()
 
@@ -60,10 +68,10 @@ def makeMenu():
     menubar = tk.Menu(Main.window)
     Main.window.config(menu=menubar)
     filemenu = tk.Menu(menubar, tearoff=0)
-    filemenu.add_command(label="New", command=lambda: print('New'))
+    filemenu.add_command(label="New", command=lambda: print("New"))
     filemenu.add_command(label="Open", command=lambda: openFile())
     filemenu.add_command(label="Save", command=lambda: fileParser.save())
-    filemenu.add_command(label="Save as...", command=lambda: print('Save as...'))
+    filemenu.add_command(label="Save as...", command=lambda: print("Save as..."))
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command=Main.window.quit)
     menubar.add_cascade(label="File", menu=filemenu)
@@ -71,14 +79,20 @@ def makeMenu():
     sortmenu.add_command(label="reset", command=lambda: resetSort())
     menubar.add_cascade(label="Sort", menu=sortmenu)
     editmenu = tk.Menu(menubar, tearoff=0)
-    editmenu.add_command(label="Add column", command=lambda: print('Add column'))
+    editmenu.add_command(label="Add column", command=lambda: print("Add column"))
     editmenu.add_command(label="Add row", command=lambda: addRow())
     menubar.add_cascade(label="Edit", menu=editmenu)
 
 
 def initWindow():
-    Main.window = OpenWindow('1000x500', 'Pyxcel')
-    Main.context = {'array': [], 'sortKey': '', 'sortReverse': False, 'file': '', 'columns': []}
+    Main.window = OpenWindow("1000x500", "Pyxcel")
+    Main.context = {
+        "array": [],
+        "sortKey": "",
+        "sortReverse": False,
+        "file": "",
+        "columns": [],
+    }
     # Menu
     makeMenu()
 
@@ -98,32 +112,34 @@ def clearWindow():
 
 def displayArray():
     clearWindow()
-    if len(Main.context['array']) == 0:
+    if len(Main.context["array"]) == 0:
         text = tk.Label(Main.window, text="No data to display")
-        text.place(relx=0.5, rely=0.5, anchor='center')
+        text.place(relx=0.5, rely=0.5, anchor="center")
     else:
         createTable()
     Main.window.update()
 
 
 def sortArray(column):
-    if Main.context['sortKey'] == column:
-        Main.context['array'].sort(key=lambda x: x[column], reverse=not Main.context['sortReverse'])
-        Main.context['sortReverse'] = not Main.context['sortReverse']
+    if Main.context["sortKey"] == column:
+        Main.context["array"].sort(
+            key=lambda x: x[column], reverse=not Main.context["sortReverse"]
+        )
+        Main.context["sortReverse"] = not Main.context["sortReverse"]
     else:
-        Main.context['array'].sort(key=lambda x: x[column])
-        Main.context['sortReverse'] = False
-    Main.context['sortKey'] = column
+        Main.context["array"].sort(key=lambda x: x[column])
+        Main.context["sortReverse"] = False
+    Main.context["sortKey"] = column
     displayArray()
 
 
 def resetSort():
-    Main.context['sortKey'] = ''
-    Main.context['sortReverse'] = False
+    Main.context["sortKey"] = ""
+    Main.context["sortReverse"] = False
     displayArray()
 
 
 def addRow():
-    Main.context['array'].append({})
-    print(Main.context['array'])
+    Main.context["array"].append({})
+    print(Main.context["array"])
     displayArray()
