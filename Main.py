ffaba5645d315df2f0bae85d23cbf65997361aad
johan <94543237+jogoml@users.noreply.github.com>
2024-02-14 18:04:13 +0100

    Add menu (#8)

---
+++ Main.py
@@ -0,0 +1,15 @@
+import Display
+import fileParser
+
+global window
+global context
+
+if __name__ == '__main__':
+    # dic = fileParser.jsonFileToArray('json.json')
+    # fileParser.arrayToXml(dic, 'xml.xml')
+    # fileParser.arrayToCsv(dic, 'csv.csv')
+    # fileParser.arrayToYaml(dic, 'yaml.yaml')
+
+
+    # Test Display
+    Display.initWindow()
