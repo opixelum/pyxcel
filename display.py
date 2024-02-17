2773aafbc0757797353b07191500ae04af4c9fe1
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-02-17 03:25:47 +0100

    Refactoring & hide `.0` from numbers (#42)
    * Remove useless field "original" in context
    
    * Rename array key to data
    
    * Set function names to snake case
    
    * Set remaining function names to snake case
    
    * Rename files to snake case
    
    * Refactor `display.py`
    
    * Refactor `file_parser.py`
    
    * Update column type when typing in value cell
    
    * Update window title after opening a file
    
    * Replace empty cell by default value
    
    * Fix empty cell type checking
    
    * Refactor `file_parser.py`
    
    * Hide `.0` at end of numbers

---
+++ display.py
@@ -0,0 +1,435 @@
+import tkinter as tk
+from tkinter import filedialog
+import file_parser
+import main
+
+
+def open_window(size, title):
+    w = tk.Tk()
+    w.geometry(size)
+    w.title(title)
+    return w
+
+
+def default_value(column_name):
+    if file_parser.column_type(main.context["data"], column_name) == int:
+        return 0
+    elif file_parser.column_type(main.context["data"], column_name) == float:
+        return 0.0
+    elif file_parser.column_type(main.context["data"], column_name) == bool:
+        return False
+    elif file_parser.column_type(main.context["data"], column_name) == list:
+        return []
+    else:
+        return ""
+
+
+def update_value(row_number, column_name, value_string_var):
+    new_value = value_string_var.get()
+
+    if new_value == "":
+        new_value = default_value(column_name)
+
+    main.context["data"][row_number][column_name] = new_value
+    main.context["data"] = file_parser.unify_column_type(
+        main.context["data"], column_name
+    )
+
+
+def revert_to_original():
+    """
+    Reverts the array to its original state (before any modification).
+    """
+    file_type = main.context["file_path"].split(".")[-1]
+    main.context["file_path"] = main.context["file_path"]
+    if file_type == "csv":
+        main.context["data"] = file_parser.csv_to_data(main.context["file_path"])
+    elif file_type == "json":
+        main.context["data"] = file_parser.json_to_data(main.context["file_path"])
+    elif file_type == "xml":
+        main.context["data"] = file_parser.xml_to_data(main.context["file_path"])
+    elif file_type == "yaml":
+        main.context["data"] = file_parser.yaml_to_data(main.context["file_path"])
+    display_data()
+
+
+def create_table():
+    number_of_rows = max(len(main.context["data"]) + 1, 10)
+    number_of_columns = len(main.context["data"][0])
+
+    for i in range(number_of_columns):
+        main.window.grid_columnconfigure(i, weight=1)
+    for i in range(number_of_rows):
+        main.window.grid_rowconfigure(i, weight=1)
+
+    # Keep track of the headers stringvars to update them on change
+    for column_number, column_name in enumerate(main.context["data"][0]):
+        header_sv = tk.StringVar(value=column_name)
+        header_entry = tk.Entry(main.window, textvariable=header_sv)
+        header_entry.bind(
+            "<FocusOut>",
+            lambda *_, previous=column_name, sv=header_sv,: update_column_name(
+                previous, sv
+            ),
+        )
+        header_entry.grid(row=0, column=column_number)
+        header_entry.bind(
+            "<Button-3>", lambda _, x=column_name: column_name_right_click_menu(x)
+        )
+
+    for row_number, row in enumerate(main.context["data"]):
+        for column_number, (column_name, value) in enumerate(row.items()):
+            # If the value is a number ending with .0, display it as an integer
+            if str(value)[-2:] == ".0":
+                value = str(value)[:-2]
+
+            value_string_var = tk.StringVar(value=value)
+            value_string_var.trace_add(
+                "write",
+                lambda *_,
+                a=row_number,
+                b=column_name,
+                c=value_string_var: update_value(a, b, c),
+            )
+            cell = tk.Entry(main.window, textvariable=value_string_var)
+            cell.grid(row=row_number + 1, column=column_number)
+            cell.bind(
+                "<Button-3>",
+                lambda _, x=column_name, row=row_number: value_right_click_menu(x, row),
+            )
+
+
+def open_file():
+    main.context = {
+        "sort_key": "",
+        "sort_reverse": False,
+    }
+
+    file = filedialog.askopenfile()
+
+    if file:
+        # get type of file csv, json, xml, yaml
+        file_type = file.name.split(".")[-1]
+
+        main.context["file_path"] = file.name
+
+        # Update window title
+        file_name = file.name.split("/")[-1]
+        main.window.title(file_name + " - Pyxcel")
+
+        if file_type == "csv":
+            main.context["data"] = file_parser.csv_to_data(file.name)
+        elif file_type == "json":
+            main.context["data"] = file_parser.json_to_data(file.name)
+        elif file_type == "xml":
+            main.context["data"] = file_parser.xml_to_data(file.name)
+        elif file_type == "yaml":
+            main.context["data"] = file_parser.yaml_to_data(file.name)
+        else:
+            print("ERROR: File type not supported")
+            main.context["data"] = []
+
+        display_data()
+
+
+def save_as():
+    file = filedialog.asksaveasfile(
+        defaultextension=".csv",
+        filetypes=[
+            ("CSV files", "*.csv"),
+            ("JSON files", "*.json"),
+            ("XML files", "*.xml"),
+            ("YAML files", "*.yaml"),
+        ],
+    )
+    if file is None:
+        return
+    file_type = file.name.split(".")[-1]
+
+    for i, row in enumerate(main.context["data"]):
+        for _, (key, _) in enumerate(row.items()):
+            row[key] = main.context["cell_vars"][i][key].get()
+
+    main.context["file_path"] = file.name
+
+    if file_type == "csv":
+        file_parser.data_to_csv(main.context["data"], file.name)
+    elif file_type == "json":
+        file_parser.data_to_json(main.context["data"], file.name)
+    elif file_type == "xml":
+        file_parser.data_to_xml(main.context["data"], file.name)
+    elif file_type == "yaml":
+        file_parser.data_to_yaml(main.context["data"], file.name)
+    else:
+        print("ERROR: File type not supported")
+        main.context["data"] = []
+
+    display_data()
+
+
+def save():
+    if main.context["file_path"] == "":
+        save_as()
+        return
+
+    file_type = main.context["file_path"].split(".")[-1]
+
+    for i, row in enumerate(main.context["data"]):
+        for _, (key, _) in enumerate(row.items()):
+            row[key] = main.context["cell_vars"][i][key].get()
+
+    if file_type == "csv":
+        file_parser.data_to_csv(main.context["data"], main.context["file_path"])
+    elif file_type == "json":
+        file_parser.data_to_json(main.context["data"], main.context["file_path"])
+    elif file_type == "xml":
+        file_parser.data_to_xml(main.context["data"], main.context["file_path"])
+    elif file_type == "yaml":
+        file_parser.data_to_yaml(main.context["data"], main.context["file_path"])
+    else:
+        print("ERROR: File type not supported")
+
+
+def create_menus():
+    menubar = tk.Menu(main.window)
+    main.window.config(menu=menubar)
+
+    filemenu = tk.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="New", command=lambda: new_file())
+    filemenu.add_command(label="Open...", command=lambda: open_file())
+    filemenu.add_command(label="Save", command=lambda: save())
+    filemenu.add_command(label="Save As...", command=lambda: save_as())
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit Pyxcel", command=main.window.quit)
+    menubar.add_cascade(label="File", menu=filemenu)
+
+    sortmenu = tk.Menu(menubar, tearoff=0)
+    sortmenu.add_command(label="Reset", command=lambda: reset_sort())
+    menubar.add_cascade(label="Sort", menu=sortmenu)
+
+    editmenu = tk.Menu(menubar, tearoff=0)
+    editmenu.add_command(
+        label="Revert to Original", command=lambda: revert_to_original()
+    )
+    editmenu.add_command(label="Add Row", command=lambda: add_row())
+    editmenu.add_command(label="Add Column", command=lambda: add_column())
+    menubar.add_cascade(label="Edit", menu=editmenu)
+
+
+def column_name_right_click_menu(header):
+    rightClickMenu = tk.Menu(main.window, tearoff=0)
+    rightClickMenu.add_command(label="Show Stats", command=lambda: show_stats(header))
+    rightClickMenu.add_command(label="Sort", command=lambda: sort_data(header))
+    rightClickMenu.add_command(
+        label="Delete Column", command=lambda: delete_column(header)
+    )
+    rightClickMenu.post(main.window.winfo_pointerx(), main.window.winfo_pointery())
+
+
+def value_right_click_menu(header, row_number):
+    rightClickMenu = tk.Menu(main.window, tearoff=0)
+    rightClickMenu.add_command(label="Show Stats", command=lambda: show_stats(header))
+    rightClickMenu.add_command(label="Sort", command=lambda: sort_data(header))
+    rightClickMenu.add_command(
+        label="Delete Row", command=lambda: delete_row(row_number)
+    )
+    rightClickMenu.add_command(
+        label="Delete Column", command=lambda: delete_column(header)
+    )
+    rightClickMenu.post(main.window.winfo_pointerx(), main.window.winfo_pointery())
+
+
+def init_window():
+    main.window = open_window("1000x500", "Pyxcel")
+    main.context = {"data": [], "sort_key": "", "sort_reverse": False, "file_path": ""}
+
+    # Shortcuts
+    main.window.bind("<Control-s>", lambda _: save())
+
+    display_data()
+    main.window.mainloop()
+
+
+def clear_window():
+    for widget in main.window.winfo_children():
+        widget.destroy()
+    main.window.update()
+
+
+def display_data():
+    clear_window()
+    create_menus()
+    if len(main.context["data"]) == 0:
+        text = tk.Label(main.window, text="No data to display.")
+        text.place(relx=0.5, rely=0.5, anchor="center")
+    else:
+        create_table()
+    main.window.update()
+
+
+def sort_data(column):
+    if main.context["sort_key"] == column:
+        main.context["data"].sort(
+            key=lambda x: x[column], reverse=not main.context["sort_reverse"]
+        )
+        main.context["sort_reverse"] = not main.context["sort_reverse"]
+    else:
+        main.context["data"].sort(key=lambda x: x[column])
+        main.context["sort_reverse"] = False
+    main.context["sort_key"] = column
+    display_data()
+
+
+def reset_sort():
+    main.context["sort_key"] = ""
+    main.context["sort_reverse"] = False
+    display_data()
+
+
+def show_stats(column):
+    display_data()
+    windowStats = open_window("300x300", "Stats")
+
+    column_type = file_parser.column_type(main.context["data"], column)
+
+    # Numbers
+    if column_type == int or column_type == float:
+        min = main.context["data"][0][column]
+        max = main.context["data"][0][column]
+        avg = 0
+
+        for i in main.context["data"]:
+            if min > i[column]:
+                min = i[column]
+            if max < i[column]:
+                max = i[column]
+            avg += i[column]
+
+        avg /= len(main.context["data"])
+
+        if str(min)[-2:] == ".0":
+            min = int(str(min)[:-2])
+
+        if str(max)[-2:] == ".0":
+            max = int(str(max)[:-2])
+
+        if str(avg)[-2:] == ".0":
+            avg = int(str(avg)[:-2])
+
+        text = tk.Label(
+            windowStats,
+            text="Min: " + str(min) + "\nMax: " + str(max) + "\nAvg: " + str(avg),
+        )
+        text.place(relx=0.5, rely=0.5, anchor="center")
+        text.pack()
+        windowStats.mainloop()
+
+    # Booleans
+    elif column_type == bool:
+        true = 0
+        false = 0
+
+        for i in main.context["data"]:
+            if i[column]:
+                true += 1
+            else:
+                false += 1
+
+        text = tk.Label(
+            windowStats,
+            text="True: "
+            + str(true / len(main.context["data"]) * 100)
+            + "%\nFalse: "
+            + str(false / len(main.context["data"]) * 100)
+            + "%",
+        )
+        text.place(relx=0.5, rely=0.5, anchor="center")
+        text.pack()
+        windowStats.mainloop()
+
+    # Lists and strings
+    elif column_type == list or column_type == str:
+        min = len(main.context["data"][0][column])
+        max = len(main.context["data"][0][column])
+        avg = 0
+
+        for i in main.context["data"]:
+            if min > len(i[column]):
+                min = len(i[column])
+            if max < len(i[column]):
+                max = len(i[column])
+            avg += len(i[column])
+
+        avg /= len(main.context["data"])
+
+        if str(min)[-2:] == ".0":
+            min = int(str(min)[:-2])
+
+        if str(max)[-2:] == ".0":
+            max = int(str(max)[:-2])
+
+        if str(avg)[-2:] == ".0":
+            avg = int(str(avg)[:-2])
+
+        min_text = (
+            "Min "
+            + ("list" if column_type == list else "string")
+            + " size: "
+            + str(min)
+        )
+        max_text = (
+            "\nMax "
+            + ("list" if column_type == list else "string")
+            + " size: "
+            + str(max)
+        )
+        avg_text = (
+            "\nAvg "
+            + ("list" if column_type == list else "string")
+            + " size: "
+            + str(avg)
+        )
+        text = tk.Label(windowStats, text=min_text + max_text + avg_text)
+        text.place(relx=0.5, rely=0.5, anchor="center")
+        text.pack()
+        windowStats.mainloop()
+
+
+def add_row():
+    main.context["data"].append({key: "" for key in main.context["data"][0]})
+    display_data()
+
+
+def add_column():
+    for row in main.context["data"]:
+        row["New column"] = ""
+    display_data()
+
+
+def update_column_name(previous, sv):
+    for row in main.context["data"]:
+        row[sv.get()] = row.pop(previous)
+
+    # Need to update the header name on the frontend
+    display_data()
+
+
+def delete_row(row_number):
+    main.context["data"].pop(row_number)
+    display_data()
+
+
+def delete_column(header):
+    for row in main.context["data"]:
+        row.pop(header)
+    display_data()
+
+
+def new_file():
+    main.context = {
+        "data": [{"New column": ""}],
+        "sort_key": "",
+        "sort_reverse": False,
+        "file_path": "",
+    }
+    display_data()
--- file_parser.py
+++ file_parser.py
@@ -11,7 +11,10 @@ def isfloat(string):
         return False
 
 
-def stringToTypeOfValue(string):
+def str_to_type(string):
+    if string == "":
+        return string
+
     if string[0] == "-" or string[0] == "+" or string[0].isdigit():
         if string.isdigit():
             return int(string)
@@ -27,37 +30,41 @@ def stringToTypeOfValue(string):
     return string
 
 
-def jsonFileToArray(name):
-    with open(name, "r") as f:
+def json_to_data(file_path):
+    with open(file_path, "r") as f:
         return json.load(f)
 
 
-def arrayToJson(arr, name):
-    with open(name, "w") as f:
+def data_to_json(arr, file_path):
+    with open(file_path, "w") as f:
         json.dump(arr, f)
 
 
-def isCsvParser(i, separator):
-    if not i.isdigit() and not i.isalpha() and i != separator and i != "\n" and i != "\r" and i != "\"" and i != "'" and i != "." and i != "!" and i != "?" and i != "@":
-        return True
-    return False
+def is_csv_parser(character, separator):
+    special_chars = {"\n", "\r", '"', "'", ".", "!", "?", "@"}
+    return (
+        not character.isdigit()
+        and not character.isalpha()
+        and character not in special_chars.union({separator})
+    )
 
-def csvToArray(name):
+
+def csv_to_data(file_path):
     res = []
 
-    with open(name, "r") as f:
+    with open(file_path, "r") as f:
         t = f.read().split("\n")
         separator = ";"
-        for i in t[0]:
-            if isCsvParser(i, separator):
-                separator = i
+        for character in t[0]:
+            if is_csv_parser(character, separator):
+                separator = character
                 break
 
         header = t[0].split(separator)
         separator2 = ""
-        for i in t[1]:
-            if isCsvParser(i, separator):
-                separator2 = i
+        for character in t[1]:
+            if is_csv_parser(character, separator):
+                separator2 = character
                 break
         for i in t[1:]:
             if i == "":
@@ -66,19 +73,19 @@ def csvToArray(name):
             d = {}
             for j, value in enumerate(i.split(separator)):
                 if separator2 == "":
-                    d[header[j]] = stringToTypeOfValue(value)
+                    d[header[j]] = str_to_type(value)
                 else:
                     if separator2 in value:
-                        d[header[j]] = [stringToTypeOfValue(k) for k in value.split(separator2)]
+                        d[header[j]] = [str_to_type(k) for k in value.split(separator2)]
             res.append(d)
 
         return res
 
 
-def arrayToCsv(array, name):
-    header = [i for i in array[0]]
+def data_to_csv(data, file_path):
+    header = [i for i in data[0]]
     r = ";".join(header)
-    for i in array:
+    for i in data:
         line = []
         for j in header:
             if isinstance(i[j], list):
@@ -86,13 +93,13 @@ def arrayToCsv(array, name):
             else:
                 line.append(str(i[j]))
         r += "\n" + ";".join(line)
-    with open(name, "w") as f:
+    with open(file_path, "w") as f:
         f.write(r)
 
 
-def arrayToXml(array, name):
+def data_to_xml(data, file_path):
     root = ET.Element("root")
-    for i in array:
+    for i in data:
         child = ET.Element("row")
         root.append(child)
         for j in i:
@@ -100,45 +107,45 @@ def arrayToXml(array, name):
             child2.text = str(i[j])
             child.append(child2)
     tree = ET.ElementTree(root)
-    tree.write(name)
+    tree.write(file_path)
 
 
-def xmlToArray(name):
-    root = ET.parse(name).getroot()
+def xml_to_data(file_path):
+    root = ET.parse(file_path).getroot()
     res = []
     for i in root:
         d = {}
         for j in i:
-            d[j.tag] = stringToTypeOfValue(j.text)
+            d[j.tag] = str_to_type(j.text)
         res.append(d)
     return res
 
 
-def yamlToArray(name):
-    with open(name, "r") as f:
+def yaml_to_data(file_path):
+    with open(file_path, "r") as f:
         arr = yaml.safe_load(f)
     return arr
 
 
-def arrayToYaml(array, name):
-    with open(name, "w") as f:
-        yaml.dump(array, f)
+def data_to_yaml(data, file_path):
+    with open(file_path, "w") as f:
+        yaml.dump(data, f)
 
 
-def columnType(array, column):
+def column_type(data, column_name):
     has_bool = False
     has_int = False
     has_float = False
 
-    for row in array:
-        row[column] = stringToTypeOfValue(str(row[column]))
-        if isinstance(row[column], str):
+    for row in data:
+        row[column_name] = str_to_type(str(row[column_name]))
+        if isinstance(row[column_name], str):
             return str
-        if isinstance(row[column], bool):
+        if isinstance(row[column_name], bool):
             has_bool = True
-        if isinstance(row[column], int):
+        if isinstance(row[column_name], int):
             has_int = True
-        if isinstance(row[column], float):
+        if isinstance(row[column_name], float):
             has_float = True
 
     if has_bool and not has_int and not has_float:
@@ -153,24 +160,24 @@ def columnType(array, column):
     return str
 
 
-def setColumnToSameType(array, column):
+def unify_column_type(data, column_name):
     """
     Set all the values in a column to the same type:
     - If there it contains only "true" or "false" then it will be converted to boolean;
     - If it contains only digits then it will be converted to int or float (depending if it has a dot or not);
     - Else, it will be left as a string.
     """
-    if columnType(array, column) == str:
-        for row in array:
-            row[column] = str(row[column])
-    elif columnType(array, column) == bool:
-        for row in array:
-            row[column] = bool(row[column])
-    elif columnType(array, column) == float:
-        for row in array:
-            row[column] = float(row[column])
-    elif columnType(array, column) == int:
-        for row in array:
-            row[column] = int(row[column])
+    if column_type(data, column_name) == str:
+        for row in data:
+            row[column_name] = str(row[column_name])
+    elif column_type(data, column_name) == bool:
+        for row in data:
+            row[column_name] = bool(row[column_name])
+    elif column_type(data, column_name) == float:
+        for row in data:
+            row[column_name] = float(row[column_name])
+    elif column_type(data, column_name) == int:
+        for row in data:
+            row[column_name] = int(row[column_name])
 
-    return array
+    return data
